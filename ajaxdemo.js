d469d9b8e639cf2f258d4be870695b7134db6846
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-19 14:35:44 -0500

    Extracting title, year and price

--- ajaxdemo.js
+++ ajaxdemo.js
@@ -2,6 +2,29 @@ function pageLoad() {
 	document.getElementById("button").onclick = loadData;
 }
 function loadData() {
-
+	$.ajax({
+		type: "GET",
+		url: "books.xml",
+		dataType: "xml",
+		success: function(data){
+			alert("file is loaded");
+			$(data).find('bookstore').each(function(){
+				$(data).find('book').each(function(){
+					var title = $(this).find("title").text();
+					var year = $(this).find("year").text();
+					var price = $(this).find("price").text();
+					var authors = "";
+					$(data).find('author').each(function(){
+						authors = authors + $(this).text();
+						alert(authors);
+					})
+				})
+					
+			})
+		},
+		error: function(){
+			alert("error loading file");
+		}
+	});
 }
 window.onload = pageLoad;
