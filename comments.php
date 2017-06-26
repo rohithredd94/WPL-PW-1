df668e6d65bca579b10004dc43df04a67ae377f8
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-26 16:55:07 -0500

    Basic functionality added

--- comments.php
+++ comments.php
@@ -1,6 +1,15 @@
 <?php
-
-	$name = $email = $gender = $comment = $website = "";
-	echo "Hello World!";
-
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  	echo htmlspecialchars($_POST["name"]);
+  	echo "<br>";
+  	echo htmlspecialchars($_POST["email"]);
+    echo "<br>";
+    echo htmlspecialchars($_POST["website"]);
+    echo "<br>";
+    echo htmlspecialchars($_POST["comments"]);
+    echo "<br>";
+    echo htmlspecialchars($_POST["gender"]);
+	}
 ?>
