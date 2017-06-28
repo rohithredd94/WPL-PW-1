997d0c2bdf97fc344c1672d2ea375abefece5792
Rohith Reddy <rohithreddyk94@gmail.com>
2017-06-28 14:18:07 -0500

    Session variables set

--- login.php
+++ login.php
@@ -1,13 +1,26 @@
 <?php
-	error_reporting(E_ALL);
+	
+  session_start();
+
+  error_reporting(E_ALL);
 	ini_set('display_errors', 1);
 
 	if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  	echo "Name: ".test_input($_POST["name"])."<br>";
-    
-  	echo "Username: ".test_input($_POST["username"])."<br>";
 
-    echo "Password: ".test_input($_POST["password"])."<br>";
+  	/*echo "Name: ".test_input($_POST["name"])."<br>";
+  	echo "Username: ".test_input($_POST["username"])."<br>";
+    echo "Password: ".test_input($_POST["password"])."<br>";*/
+    if (!empty($_POST["name"]) && !empty($_POST["username"]) && !empty($_POST["password"])) {
+
+      $_SESSION['sess_name'] = test_input($_POST["name"]);
+      $_SESSION['sess_username'] = test_input($_POST["username"]);
+      $_SESSION['sess_password'] = test_input($_POST["password"]);
+      header('Location: welcome.php');
+
+    } else {
+      header('Location: login.html');
+    }
+
 	}
   function test_input($data) {
     $data = trim($data);
