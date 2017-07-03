363fe74e89fc5bfb661d07352cacf2db9bc96bdc
Rohith Reddy <rohithreddyk94@gmail.com>
2017-07-03 18:55:06 -0500

    Minor improvements

--- login.php
+++ login.php
@@ -2,8 +2,6 @@
     session_start();
 
     $con = mysqli_connect("localhost","root","root","pw8",3310);
-    $x = 1;
-    //var_dump(mysqli_connect_errno())
     if(mysqli_connect_errno()){
         echo "Failed to Connect to mysql".mysql_connect_error();
     }else{
@@ -16,23 +14,21 @@
         if(!empty($_POST["username"]) && !empty($_POST["password"])){
             $username = $_POST["username"];
             $password = $_POST["password"];
-            echo $password;
             //$username = mysql_real_escape_string($username);
             $query = "SELECT username, password, fullname, avatar FROM users WHERE username = '$username';";
-            //$query = "SELECT * FROM users;";
             $result = mysqli_query($con, $query);
-            var_dump($result);
             if($result->num_rows == 0){
-                echo "User not found";
+                //echo "User not found";
                 header('Location: login.html');
+                exit();
             }
             $userdata = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            var_dump($userdata);
             //$hash = hash('sha256', hash('sha256', $password));
             //echo $hash;
             if($password != $userdata['password']){
-                echo "Incorrect password";
+                //echo "Incorrect password";
                 header('Location: login.html');
+                exit();
             }else{
                 session_regenerate_id();
                 $_SESSION['username'] = $userdata['username'];
@@ -40,13 +36,16 @@
                 $_SESSION['avatar'] = $userdata['avatar'];
                 session_write_close();
                 header('Location: welcome.php');
+                exit();
             }
         }else{
-            echo "Input fields missing";
+            //echo "Input fields missing";
             header('Location: login.html');
+            exit();
         }
     }else{
         header('Location: login.html');
+        exit();
     }
 
     mysqli_close($con);
